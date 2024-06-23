494c2b6c6596589418f3414f04b66764b09ed8d8
Rajan Kumar <raorajan9576@gmail.com>
2024-06-23 13:36:15 +0530

    Server Started

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("first")
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Server is Running! 🚀");
+});
+
+module.exports = app;
